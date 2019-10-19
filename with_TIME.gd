199358acd9750473dd8da4f624f247ceadc109d0
wandom.pewlin <wandom.pewlin@gmail.com>
2019-10-19 23:41:20 +0800

    Changes random spawn volume range.

--- with_TIME.gd
+++ with_TIME.gd
@@ -3,10 +3,9 @@ extends Spatial
 func _ready():
 	for i in range($Spheres.multimesh.instance_count):
 		var position = Transform()
-		position = position.translated(Vector3(randf() * 100 - 50, randf() * 50 - 25, randf() * 50 - 25))
+		position = position.translated(Vector3(randf() * 100 - 75, randf() * 75 - 25, randf() * 75 - 25))
 		$Spheres.multimesh.set_instance_transform(i, position)
 		$Spheres.multimesh.set_instance_custom_data(i, Color(randf(), randf(), randf(), randf()))
-
 
 var time = 0.0
 func _process(delta):
